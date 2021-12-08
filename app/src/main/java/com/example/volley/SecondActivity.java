e18f8c229bf2f129c3c2b7e2b6b8ceac4e164a73
Jibinkp <jibinkp770@gmail.com>
2021-12-09 03:47:20 +0530

    added volley,and requestqueue

--- app/src/main/java/com/example/volley/SecondActivity.java
+++ app/src/main/java/com/example/volley/SecondActivity.java
@@ -2,13 +2,113 @@ package com.example.volley;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SecondActivity extends AppCompatActivity {
+    private EditText etName, etPhone;
+    private Button btnSubmit;
+    private String ipAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        initView();
     }
+
+    private void initView() {
+        etName = findViewById(R.id.et_name);
+        etPhone = findViewById(R.id.et_phone);
+        btnSubmit = findViewById(R.id.btn_submit);
+        btnSubmit.setOnClickListener(_click);
+
+        SharedPreferences sh = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        ipAddress = sh.getString("ip_address", "");
+    }
+
+    private final View.OnClickListener _click = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.btn_submit: {
+                    Toast.makeText(getApplicationContext(), "" + ipAddress, Toast.LENGTH_SHORT).show();
+                    String url = "http://" + ipAddress + ":5000/android_cust_signup";
+                    Toast.makeText(getApplicationContext(), url, Toast.LENGTH_LONG).show();
+
+                    RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+                    StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    //  Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+
+                                    // response
+                                    try {
+                                        JSONObject jsonObj = new JSONObject(response);
+                                        if (jsonObj.getString("status").equalsIgnoreCase("ok")) {
+                                            //if response success
+                                        }
+
+                                        else {
+                                            Toast.makeText(getApplicationContext(), "Not found", Toast.LENGTH_LONG).show();
+                                        }
+
+                                    } catch (Exception e) {
+                                        Toast.makeText(getApplicationContext(), "Error" + e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            },
+                            new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    // error
+                                    Toast.makeText(getApplicationContext(), "eeeee" + error.toString(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                    ) {
+                        @Override
+                        protected Map<String, String> getParams() {
+                            SharedPreferences sh = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                            Map<String, String> params = new HashMap<String, String>();
+
+
+                            params.put("name", etName.getText().toString());
+                            params.put("phone", etPhone.getText().toString());
+
+                            return params;
+                        }
+                    };
+
+                    int MY_SOCKET_TIMEOUT_MS = 100000;
+
+                    postRequest.setRetryPolicy(new DefaultRetryPolicy(
+                            MY_SOCKET_TIMEOUT_MS,
+                            DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                            DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+                    requestQueue.add(postRequest);
+
+                }
+            }
+        }
+    };
 }
